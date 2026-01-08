f81e6897f67d309384463cf78cad7d4b82c4adf2
Cika <cikazhrasy@gmail.com>
2026-01-08 11:21:22 +0700

    add lib and testing path

--- laras.gd
+++ laras.gd
@@ -1,23 +1,11 @@
 extends CharacterBody2D
 
 @export var path_follow: PathFollow2D
-@export var speed := 80.0
+@export var speed := 50.0
 
-var last_dir := Vector2.DOWN
-
-func _physics_process(delta:):
+func _physics_process(delta):
 	if path_follow == null:
 		return
-	var prev_pos	:= global_position
-	
-# maju ke path
-path_follow.progress += speed * delta
-global_position =
-path_follow.global_position
 
-#arah gerak yang bener
-var dir := global_position - prev_pos
-
-if dir != Vector2.ZERO:
-	last_dir = dir
-	play walk
+	path_follow.progress += speed * delta
+	global_position = path_follow.global_position
